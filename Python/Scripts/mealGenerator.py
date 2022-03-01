b5529cb6f9e4f83aa60992f91eb56f0eb91c098a
Khallid Williams <williams_khallid@yahoo.com>
2022-03-01 02:09:13 -0600

    Added meal generator WIP for GUI

--- Python/Scripts/mealGenerator.py
+++ Python/Scripts/mealGenerator.py
@@ -1,10 +1,27 @@
+from tkinter import *
+from tkinter.ttk import * 
+from tkinter import messagebox
+from tkinter import simpledialog
 import random as r
+
+###################### GUI ######################
+
+root = Tk() # create window
+root.title('Khallid Konnections Meal Generator')
+root.geometry('600x800')
+
+canvas1 = Canvas(root, bg='white')
+canvas1.pack(fill=BOTH, expand=True)
+
+canvas2 = Canvas(root, bg='black')
+canvas2.pack(fill=BOTH, expand=True)
+
+###################### Functionality ######################
 
 proteinList = ['Chicken', 'Ground Turkey', 'Shrimp', 'Salmon']
 veggiesList = ['Brocolli', 'Zucchini', 'Spinach', 'Kale']
 carbsList = ['Sweet Potatoes', 'Noodles', 'Jasmine Rice', 'Toast']
 drink = ['Protein Drink']
-
 breakfastList = ['Eggs & toast', 'Oatmeal']
 
 def dailyMeals():
@@ -15,18 +32,29 @@ def dailyMeals():
 
     breakfastSentence = 'For breakfast I will be eatiing {}'.format(breakfast)
     lunchSentence = 'For lunch I will be eatiing a healthy serving of {}, {}, {} with a {}'.format(meats,veggies,carbs,drink[0])
-    dinnerSentence = 'For dinner I will be eatiing a healthy serving of {} {} {}'.format(meats,veggies,carbs)
 
-    print(breakfastSentence)
-    print(lunchSentence)
-    print(dinnerSentence + '\n')
+    m = r.choice(proteinList)
+    v = r.choice(veggiesList)
+    c = r.choice(carbsList)
+
+    dinnerSentence = 'For dinner I will be eatiing a healthy serving of {} {} {}'.format(m,v,c)
+
+    return breakfastSentence + '\n' + lunchSentence + '\n' + dinnerSentence
     
+    '''
     p = meats
 
     proteinList.remove(p)
     print(breakfastSentence)
     print(lunchSentence)
-    print(dinnerSentence + '\n')
+    print(dinnerSentence + '\n
+    '''
 
+###################### BUTTONS ######################
 
-dailyMeals()
+button1 = Button (root, text='Click Meal Generator', command=dailyMeals())
+canvas1.create_window(200, 200, window=button1)
+
+Label(canvas2, text=dailyMeals()).pack()
+
+root.mainloop()
