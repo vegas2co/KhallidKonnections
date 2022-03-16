ddbb24533a96a29326468e211e1aed146c221695
Khallid Williams <williams_khallid@yahoo.com>
2022-03-15 23:23:23 -0500

    Added new code while tutoring ketorie

--- Python/Scripts/practice.py
+++ Python/Scripts/practice.py
@@ -36,17 +36,48 @@ a,b,c = hourly_employee_input()
 weekly_pay(a,str(b),str(c))
 '''
 
-def numberRange():
-  food = []
-  user_input = int(input('Enter a number between 1 and 100: '))
+#Mar 15th 2022 7:00PM
 
-  while user_input not in range(1,100):
-    food.append(user_input)
-    print(food)
-    user_input = int(input('Another Attempt -> Enter a number between 1 and 100 : '))
+def in_set(incoming_set,user_number):
+  if user_number in incoming_set:
+    boolValue = user_number in incoming_set
+    print('The boolean value is {}'.format(boolValue))
+    print('The value {} is in the set {}'.format(user_number,incoming_set))
   else:
-    food.append(user_input)
-    print(food)
+    boolValue = user_number in incoming_set
+    print('The boolean value is {}'.format(boolValue))
+    print('The value {} is not in the set {}'.format(user_number,incoming_set))
 
-numberRange()
-	
+def get_test_scores():
+  scores_dict = dict()
+  num_scores = input('Please enter number of test scores: ')
+  
+  i = 1
+  while i < int(num_scores):
+    score = input('Please enter test score:')
+    print(str(score))
+    scores_dict["Test Score"+str(i)] = int(score)
+    i+=1
+
+  return scores_dict
+
+def avg_scores(test_scores):
+  print(test_scores)
+  print('The number of scores equal {}'.format(len(test_scores)))
+  sum = 0
+  for i in test_scores.values():
+    sum = int(i) + sum
+  avg = sum/len(test_scores)
+  print('The average score is {}'.format(str(avg)))
+
+
+if __name__ == "__main__":
+  test_set = {1,1,2,3,2,4,3,4}
+  test_value = 5
+  in_set(test_set,test_value)
+
+  avg_scores(get_test_scores())
+
+
+
+
